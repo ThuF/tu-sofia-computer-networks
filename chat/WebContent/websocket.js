c3601e418c538140269d390c892b2f9eb2652090
ThuF <jordan_p_pavlov@abv.bg>
2014-12-26 17:48:30 +0200

    Fixes #12 Enhance with Bootstrap theme "Chat" example

--- chat/WebContent/websocket.js
+++ chat/WebContent/websocket.js
@@ -1,40 +1,46 @@
-var wsUri = "wss://" + document.location.hostname + ":" + document.location.port
-		+ document.location.pathname + "chat";
+var wsUri = "wss://" + document.location.hostname + ":" + document.location.port + document.location.pathname + "chat";
 var websocket = new WebSocket(wsUri);
 var username;
+
 websocket.onopen = function(evt) {
-	onOpen(evt)
+	onOpen(evt);
 };
+
 websocket.onmessage = function(evt) {
-	onMessage(evt)
+	onMessage(evt);
 };
+
 websocket.onerror = function(evt) {
-	onError(evt)
+	onError(evt);
 };
+
 var output = document.getElementById("output");
 
 function join() {
 	username = textField.value;
 	websocket.send(username + " joined");
 }
-function send_message() {
+
+function sendMessage() {
 	websocket.send(username + ": " + textField.value);
 }
+
 function onOpen() {
 	writeToScreen("Connected to " + wsUri);
 }
+
 function onMessage(evt) {
 	if (evt.data.indexOf("joined") != -1) {
-		userField.innerHTML += evt.data.substring(0, evt.data
-				.indexOf(" joined"))
-				+ "\n";
+		userField.innerHTML += evt.data.substring(0, evt.data.indexOf(" joined")) + "\n";
 	} else {
 		chatlogField.innerHTML += evt.data + "\n";
 	}
 }
+
 function onError(evt) {
 	writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
 }
+
 function writeToScreen(message) {
 	output.innerHTML += message + "<br>";
 }
